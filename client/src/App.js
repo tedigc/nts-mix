a378e8945fec6a98fe7f4840e3346e34ac9449c0
tedigc <tedigc@gmail.com>
2017-11-12 01:35:37 +0000

    Added basic tracklist searching

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,42 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { url : '' };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ url : event.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    const { url } = this.state;
+    axios.post('/api/submit', { url })
+      .then((result) => {
+        const { tracklist } = result.data;
+        for(let track of tracklist) {
+          console.log(track);
+        }
+      });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" name="nts-link" value={this.state.url} onChange={this.handleChange} />
+          <button>Submit</button>
+        </form>
       </div>
     );
   }
+
 }
 
 export default App;
--- server/src/index.js
+++ server/src/index.js
@@ -6,15 +6,45 @@ import express    from 'express';
 import morgan     from 'morgan';
 import path       from 'path';
 import bodyParser from 'body-parser';
+import request    from 'request';
+import cheerio    from 'cheerio';
 
 let port = process.env.PORT || 8080; 
 let app  = express();
 
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, '../../client/build')));
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+
+// submit a URL
+app.post('/api/submit', (req, res) => {
+  console.log(req.body.url);
+  request.get(req.body.url, (err, response, body) => {
+    if(err) {
+      console.log("error");
+      res.status(500).json(err);
+    } else {
+
+      const $ = cheerio.load(body);
+      let element = $('.shows')['0'];
+      let tracklist = [];
+
+      for(let i=1; i<element.children.length; i+=2) {
+        let trackTitle = element.children[i].children[0].data;
+        tracklist.push(trackTitle);
+      }
+
+      res.status(200).json( { 
+        success : true,
+        tracklist 
+      });  
+    }
+  });
+});
 
 // serve up web application
-app.get('/*', function(req, res) {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '../../client/build/index.html'));
 });
 
